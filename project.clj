351d96bd00a9cbd91877d5a267e50c79a0a0ab89
Joe Townsend <joe@metail.co.uk>
2016-01-08 12:43:14 +0000

    corrected project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject advent "0.1.0-SNAPSHOT"
   :description "Code to answer advent of code questions"
-  :url "https://github.org/jat45/advent"
+  :url "https://github.com/jat45/advent"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
